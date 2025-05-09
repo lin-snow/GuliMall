155c39652533b4e6a41db450a2ba53c853ce25ca
L1nSn0w <l1nsn0w@qq.com>
2025-05-09 17:45:53 +0800

    renren fast增加nacos服务注册

--- renren-fast/src/main/java/io/renren/RenrenApplication.java
+++ renren-fast/src/main/java/io/renren/RenrenApplication.java
@@ -10,9 +10,9 @@ package io.renren;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-// @EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RenrenApplication {
 
